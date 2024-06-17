024da446535de00bc0a5d53d86e44de39e04df2f
pawel <pawel.woszczyna05@gmail.com>
2024-06-17 18:37:30 +0200

    sectors created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: 'main', component: MainComponent },
+    { path: 'main', component: MainComponent, children: [
+        { path: 'home', component: HomeComponent},
+    ] },
 ];
